547aa85ccd8e5f4552359dfd9152dc2ec1cbcfda
Menno <mennoboogaard@kpnmail.nl>
2019-06-03 15:41:53 +0200

    Work on W wing

--- A22DSE/Parameters/Par_Class_Conventional.py
+++ A22DSE/Parameters/Par_Class_Conventional.py
@@ -22,7 +22,7 @@ from A22DSE.Models.Class_II_Weight.tailsizing import (ctail,ttail)
 from A22DSE.Models.POPS.Current.payloadcalculations import InletArea,\
 BurnerMass,PayloadtankVolume,PayloadtankLength,PayloadtankMass
 
-from A22DSE.Models.STRUC.current.loadingdiagram import V_Diagram,Eliptical
+#from A22DSE.Models.STRUC.current.Loadingdiagram import V_Diagram,Eliptical
 
 from A22DSE.Models.Class_II_Weight.SC_curve_and_cg import oecg
 from A22DSE.Models.STRUC.current.Class_II.FuselageLength import (
@@ -45,6 +45,7 @@ Conv.ParStruc.LG_weight_main  = Class_II_Weight_LG(Conv)
 Conv.ParStruc.Wing_weight = Basic_Wing(Conv)
 Layout.x_lemac, Conv.ParStruc.Weight_FusGroup, Layout.xcg_fuselagegroup = oecg(Conv)
 
+
  
 #preliminairy positions for tricycle landing gear (nose and main)
 Conv.ParLayoutConfig.lg_l_main,Conv.ParLayoutConfig.lg_l_nose,\
@@ -53,8 +54,6 @@ Conv.ParLayoutConfig.lg_x_nose_min_F_n, Conv.ParLayoutConfig.lg_x_nose_max_F_n,\
 Conv.ParLayoutConfig.lg_x_nose,Conv.ParLayoutConfig.lg_y_nose,\
 Conv.ParLayoutConfig.z_cg = PositionsLG_Tri(Conv)
 
-Conv.ParLayoutConfig.m_engine = 5000 # [kg] DUMMY VALUE
-Conv.ParLayoutConfig.y_engine = Conv.ParAnFP.b/2*0.25 #[m] engine at 25%
 
 
 
--- Plotting/STRUC/Loadingplot.py
+++ Plotting/STRUC/Loadingplot.py
@@ -5,27 +5,19 @@ Created on Tue May 21 09:21:46 2019
 @author: menno
 """
 
-Cruise=False
-LowSpeed=False
-
-
-
-Cruise=True
-#LowSpeed=True
 
 
 
 
 import matplotlib.pyplot as plt
-plt.clf()
 import numpy as np
 import sys
 sys.path.append('../../')
+from A22DSE.Models.STRUC.current.Loadingdiagram import Loading_Diagrams
 from A22DSE.Parameters.Par_Class_Diff_Configs import Conv
-from A22DSE.Models.STRUC.current.loadingdiagram import V_Diagram
-anfp=Conv.ParAnFP
 
-plt.plot(V_Diagram(Conv))
+
+plt.plot(Loading_Diagrams(Conv)[1])
 plt.show()
 
 
