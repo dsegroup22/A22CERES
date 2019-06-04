87c173b9cb0e906575749f03409444093ea33120
Menno <mennoboogaard@kpnmail.nl>
2019-06-04 12:06:38 +0200

    Work on fuselageweight

--- A22DSE/Models/Class_II_Weight/Detailed_Class_II_Fuselage.py
+++ A22DSE/Models/Class_II_Weight/Detailed_Class_II_Fuselage.py
@@ -27,6 +27,11 @@ def FuselageWeight(Aircraft):
     MTOMlbs = struc.MTOW/Conv.ConversTool.lbs2kg
     q_D = 0.5*Atmos.rho*(1.4*anfp.V_cruise)**2*anfp.S
     
+    #roskam: equation 5.26
+    W_f=2*10.43*K_inl**1.42*(q_D/100)**0.283*(MTOMlbs/1000)**0.95*\
+    (l_fuselage/h_fuselage)**0.71
+    
+    
 #    torenbeek method    
 #    l_ref=1.5 #[m]
 #    n_ult=2.5
@@ -39,7 +44,7 @@ def FuselageWeight(Aircraft):
 #    W_bulkheads=C_shell*d_fuselage**2*l_ref
 #    W_fl=Omega_fl*n_ult**0.5*d_fuselage*l_fuselage
 #    W_fus=W_shell+W_bulkheads+W_fl
-    return 
+    return W_f*Conv.ConversTool.lbs2kg
 =======
 
 
