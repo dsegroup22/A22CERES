2509244b07a4b577b7f639ed25dd68aa38faf235
dsegroup22 <50166265+dsegroup22@users.noreply.github.com>
2019-06-04 11:29:48 +0200

    Update Detailed_Class_II_Fuselage.py

--- A22DSE/Models/Class_II_Weight/Detailed_Class_II_Fuselage.py
+++ A22DSE/Models/Class_II_Weight/Detailed_Class_II_Fuselage.py
@@ -5,7 +5,9 @@ Created on Mon Jun  3 09:30:41 2019
 @author: menno
 """
 import numpy as np
+from A22DSE.Parameters.Par_Class_Atmos import Atmos
 
 
 def FuselageWeight(Aircraft):
-    
+    anfp = Aircraft.ParAnFP
+    q_D = 0.5*Atmos.rho*(1.4*anfp.V_cruise)**2*anfp.S
