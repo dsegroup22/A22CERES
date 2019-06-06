1dfd8962f3ae47f6c5897739cfffc456f76a01fc
kslam07 <trykslam@gmail.com>
2019-06-06 09:51:08 +0200

    Update Planform

--- A22DSE/Models/AnFP/Current/Class_II/WingDesign/PlanformMain.py
+++ A22DSE/Models/AnFP/Current/Class_II/WingDesign/PlanformMain.py
@@ -32,31 +32,31 @@ W_num = 10000
 W_denum = 0.76
 sweep = np.deg2rad(5)
 
-def GetMTOW(CL_eqi, MTOWi, Awi):
-
-    q_des = FunctionsPlanform.DynamicPressEq(Conv, ISA_model)
-#    Fprop = FunctionsPlanform.ComputeFprop(Conv, ISA_model, MTOWi)
-    Fprop = 8.5
-    CDp   = FunctionsPlanform.CDpCurlFunc(Conv, ISA_model, sweep)
-    CDpS  = CDp*Conv.ParAnFP.S
-    Theta1= FunctionsPlanform.ComputeTheta1(Conv, ISA_model)
-    Theta2= FunctionsPlanform.ComputeTheta2(Conv, ISA_model)
-    FWP   = FunctionsPlanform.FWP_subsonic(Theta1, Theta2, Conv, ISA_model, 
-                                           Awi, MTOWi, CL_eqi)
-    
-    
-    y = MTOWi - (MTOWi + CDpS*q_des*Fprop)/(W_denum + FWP)
-    return y
-
-Z = np.ones([np.size(Aw), np.size(CL_eq)])
-for i, MTOWi in enumerate(MTOW):
-    for j, Awi in enumerate(Aw):
-#        print (CL_eqi)
-        
-       Z[i,j] = fsolve(GetMTOW, .75, args=(MTOWi, Awi))
-    
-X,Y = np.meshgrid(MTOW,Aw)
-
+#def GetMTOW(CL_eqi, MTOWi, Awi):
+#
+#    q_des = FunctionsPlanform.DynamicPressEq(Conv, ISA_model)
+##    Fprop = FunctionsPlanform.ComputeFprop(Conv, ISA_model, MTOWi)
+#    Fprop = 8.5
+#    CDp   = FunctionsPlanform.CDpCurlFunc(Conv, ISA_model, sweep)
+#    CDpS  = CDp*Conv.ParAnFP.S
+#    Theta1= FunctionsPlanform.ComputeTheta1(Conv, ISA_model)
+#    Theta2= FunctionsPlanform.ComputeTheta2(Conv, ISA_model)
+#    FWP   = FunctionsPlanform.FWP_subsonic(Theta1, Theta2, Conv, ISA_model, 
+#                                           Awi, MTOWi, CL_eqi)
+#    
+#    
+#    y = MTOWi - (MTOWi + CDpS*q_des*Fprop)/(W_denum + FWP)
+#    return y
+#
+#Z = np.ones([np.size(Aw), np.size(CL_eq)])
+#for i, MTOWi in enumerate(MTOW):
+#    for j, Awi in enumerate(Aw):
+##        print (CL_eqi)
+#        
+#       Z[i,j] = fsolve(GetMTOW, .75, args=(MTOWi, Awi))
+#    
+X1,Y = np.meshgrid(MTOW,Aw)
+X2,Z = np.meshgrid(MTOW, CL_eq)
 
 # =============================================================================
 #                          Constraint I: Optimum CL
@@ -93,14 +93,14 @@ for i, MTOWi in enumerate(MTOW):
 #
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Z, Y)                         #MTOW, CL, Aw
-ax.plot_surface(X, CL_des, Y)
-ax.plot_surface(X, CL_eq, Aw_des)
+#ax.plot_surface(X, Z, Y)                         #MTOW, CL, Aw
+ax.plot_wireframe(X1, CL_des, Y)
+ax.plot_wireframe(X2, Z, Aw_des)
 
-
+#
 fig, ax = plt.subplots()
-CS = ax.contour(X, CL_des, Y)
-CT = ax.contour(X,CL_eq, Aw_des)
-ax.clabel(CS, inline=1, fontsize=10)
+CS = ax.contour(X1, CL_des, Y)
+CT = ax.contour(X2, Z, Aw_des)
+ax.clabel(CT, inline=1, fontsize=10)
 
 
