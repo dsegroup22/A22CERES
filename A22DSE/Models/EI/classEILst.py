a4201a7362de736f7b1d28c2c8c4efdf3e98c955
dsegroup22 <50166265+dsegroup22@users.noreply.github.com>
2019-06-07 11:14:18 +0200

    Changed EI

--- A22DSE/Models/EI/classEILst.py
+++ A22DSE/Models/EI/classEILst.py
@@ -27,24 +27,29 @@ class pollutant(object):
     
     def __init__(self, name, GWPLst, RFLst):
         
-        self.name = str(name)
+#        self.name = str(name)
         self.GWP  = GWPLst
         self.RF = RFLst
+        
         
         
 class pollutantLst(object):
     
     def __init__(self):
         
-        self.pollutantsLst = []
+        self.CO2 = pollutant('CO2', np.array([1,1,1]), 1.985)
+        self.CH4 = pollutant('CH4', np.array([63,21,9]), 0.507)
+        self.N2O = pollutant('N2O', np.array([270,290, 190]), 0.983)
+        self.CFC11 = pollutant('CFC11', np.array([4500,3500,1500]), 0.057)
+        self.CFC22 = pollutant('CFC12', np.array([4100, 1500, 510]), 0.164)
+        self.HCFC22 = pollutant('HCFC22', np.array([4100, 1500, 510]), None)
+        self.N2 = pollutant('N2', np.array([None,None,None]), None)
+        self.O2 = pollutant('O2', np.array([None,None,None]), None)
+        self.H2O = pollutant('H2O', np.array([None,None,None]), None)
+        self.N2 = pollutant('N2', np.array([None,None,None]), None)
+        self.CO = pollutant('CO', np.array([None,None,None]), None)
+        self.H2 = pollutant('H2', np.array([None,None,None]), None)
         
         
-CO2 = pollutant('CO2', np.array([1,1,1]), 1.985)
-CH4 = pollutant('CH4', np.array([63,21,9]), 0.507)
-N2O = pollutant('N2O', np.array([270,290, 190]), 0.983)
-CO2 = pollutant('CFC11', np.array([4500,3500,1500]), 0.057)
-CO2 = pollutant('CFC12', np.array([4100, 1500, 510]), 0.164)
-CO2 = pollutant('HCFC22', np.array([4100, 1500, 510]), None)
-    
         
     
