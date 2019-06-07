17f84f5ebf2780153000e660a08a9f292ae83e21
dsegroup22 <50166265+dsegroup22@users.noreply.github.com>
2019-06-07 12:01:56 +0200

    Update Class_II_Wing.py

--- A22DSE/Models/Class_II_Weight/Class_II_Wing.py
+++ A22DSE/Models/Class_II_Weight/Class_II_Wing.py
@@ -41,8 +41,8 @@ def Wing_Geo(Aircraft):
     S = anfp.S
     
     Sweep_50 = Sweep_50 * pi / 180.
-    Taper = 0.45 * exp( -0.0375 * Sweep_50_rad) 
-    Sweep_25_rad = atan( tan(Sweep_50_rad) + (4 / AR) * 0.25 \
+    Taper = 0.45 * exp( -0.0375 * Sweep_50) 
+    Sweep_25_rad = atan( tan(Sweep_50) + (4 / AR) * 0.25 \
                     * (1 - Taper) / (1 + Taper))
     Sweep_LE = atan( tan(Sweep_25_rad) - (4 / AR) * -0.25 \
                     * (1 - Taper) / ( 1 + Taper))
