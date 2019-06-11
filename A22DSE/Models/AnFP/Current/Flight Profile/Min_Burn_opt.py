ff1d18148bdc1e9294b5d4573bfd53387e51237f
dsegroup22 <50166265+dsegroup22@users.noreply.github.com>
2019-06-11 14:34:58 +0200

    Update Min_Burn_opt.py

--- A22DSE/Models/AnFP/Current/Flight Profile/Min_Burn_opt.py
+++ A22DSE/Models/AnFP/Current/Flight Profile/Min_Burn_opt.py
@@ -29,10 +29,18 @@ def getatm(h):
     rho = np.multiply(p ,1/(0.2869*(T+273.1)))
     return T, rho, p
 
-SFC = 22 #g/s/kN
+SFC = 26.25  * 10**-6 #kg/s/N
 MThrust = 80*10**3 #N
 
 n_engines = 6
+
+W = 65000*9.81 #N
+
+S = 400
+CD0 = 0.01
+CL = 0.7*0.86
+A = 13
+e = 0.75
 
 """Minimum time climb"""
 #Max RC
@@ -51,13 +59,7 @@ for i in range(len(MaxT[0])):
 
 
 He = np.ravel(H) + np.power(np.ravel(V),2)/2/9.81
-W = 60000*9.81 #N
 
-S = 400
-CD0 = 0.01
-CL = 0.7
-A = 17
-e = 0.75
 
 RCs = (np.ravel(MaxT)*n_engines-0.5*np.ravel(rho)*np.power(np.ravel(V),2)*S*\
        (CD0+CL**2/m.pi/A/e))*np.ravel(V)/W
@@ -67,9 +69,10 @@ He = He.reshape(shape)
 H = H.reshape(shape)
 V = V.reshape(shape)     
 
+
+
 tclimb=np.zeros((res))
 
-He_tmin = np.sort(np.ravel(He))
 He_ar = np.linspace(2000,22162.895,res1)
 RCs_tmin = np.zeros(res1)
 V_tmin = np.zeros(res1)
@@ -86,9 +89,19 @@ for i in range(len(He_ar)):
     
 #compute mdot/RCs
 
-
-
-       
+#He_opt = np.linspace(2000,22162.895,res1)
+#RCs_opt = np.zeros(res1)
+#V_opt = np.zeros(res1)
+#H_opt = np.zeros(res1)
+#for i in range(len(He_opt)):
+#    RCs_opt[i] = np.amax(RCs[np.where(np.logical_and(He_opt[i]> He-z/res , He_opt[i] < He+z/res))])
+#    index=(int(np.where(RCs == np.amax(RCs[np.where(\
+#        np.logical_and(He_ar[i]> He-z/res , He_ar[i] < He+z/res))]))[0]),\
+#    int(np.where(RCs == np.amax(RCs[np.where(\
+#        np.logical_and(He_ar[i]> He-z/res , He_ar[i] < He+z/res))]))[1]))
+##    print(index)
+#    V_tmin[i] = V[index]
+#    H_tmin[i] = H[index]       
        
 #integrate         
        
@@ -113,18 +126,12 @@ a = plt.contour(np.power(V,2)/2/9.81,H,He,5,colors='k', linewidths = 0.5)
 b = plt.contour(np.power(V,2)/2/9.81,H,RCs,20,colors='k')
 plt.plot(np.power(V_tmin,2)/2/9.81,H_tmin)
 plt.plot(206**2/2/9.81,20000,'k o')
+plt.plot(np.ones(res)*129**2/2/9.81,H[:,0])
 plt.clabel(b, inline=1, fontsize=10)
 plt.show()
 
+
 #plt.figure(2)
-#a =plt.contour(np.power(V,2)/2/9.81,H,dRCdV)
-#plt.clabel(a, inline=1, fontsize=10)
+#a = plt.plot(He_ar, np.divide(1,RCs_tmin))
+#plt.title('Climb time ='+ str(float(np.trapz(np.divide(1,RCs_tmin),He_ar))/60)+'min')
 #plt.show()
-
-#plt.figure(3)
-#a = plt.scatter(He,invRCs)
-#a = plt.scatter(np.linspace(0,22162.895,res),1/RCs_tmin, color = 'k')
-#plt.show()
-
-#plt.figure(4)
-#a = plt.plot()
