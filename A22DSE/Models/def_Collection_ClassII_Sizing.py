26b442d37fa3f4cfafe06eb48de091aa05ce23de
lujingyi <50166265+dsegroup22@users.noreply.github.com>
2019-06-11 23:35:08 +0900

    append minimum load factor

--- A22DSE/Models/def_Collection_ClassII_Sizing.py
+++ A22DSE/Models/def_Collection_ClassII_Sizing.py
@@ -44,7 +44,7 @@ def ClassIISizing(Conv):
 # =============================================================================
 #                           WING PLANFORM DESIGN
 # =============================================================================
-    Conv.ParAnFP.n_ult, Conv.ParAnFP.V_stall, Conv.ParAnFP.V_dive = flightenvelope(Conv)
+    Conv.ParAnFP.n_ult, Conv.ParAnFP.V_stall, Conv.ParAnFP.V_dive, Conv.ParAnFP.n_lim = flightenvelope(Conv)
     
 #    Conv.ParAnFP.C_L_design, Conv.ParAnFP.A = PlanformMain.GetARTransWing(
 #    Conv, ISA_model, step, False)
