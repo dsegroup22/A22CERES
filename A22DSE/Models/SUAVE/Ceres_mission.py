4702c265e91636c4f7daf06182ee82e37b21d41a
ThomasRV <thomas.verduyn@gmail.com>
2019-06-11 14:59:46 +0200

    suave

--- A22DSE/Models/SUAVE/Ceres_mission.py
+++ A22DSE/Models/SUAVE/Ceres_mission.py
@@ -596,38 +596,38 @@ def configs_setup(vehicle):
 
     return configs
 
-#def simple_sizing(configs):
-#
-#    base = configs.base
-#    base.pull_base()
-#
-#    # zero fuel weight
-#    base.mass_properties.max_zero_fuel = 0.9 * base.mass_properties.max_takeoff 
-#
-#    # wing areas
-#    for wing in base.wings:
-#        wing.areas.wetted   = 2.0 * wing.areas.reference
-#        wing.areas.exposed  = 0.8 * wing.areas.wetted
-#        wing.areas.affected = 0.6 * wing.areas.wetted
-#
-#    # diff the new data
-#    base.store_diff()
-#
-#    # ------------------------------------------------------------------
-#    #   Landing Configuration
-#    # ------------------------------------------------------------------
-#    landing = configs.landing
-#
-#    # make sure base data is current
-#    landing.pull_base()
-#
-#    # landing weight
-#    landing.mass_properties.landing = 0.85 * base.mass_properties.takeoff
-#
-#    # diff the new data
-#    landing.store_diff()
-#
-#    return
+def simple_sizing(configs):
+
+    base = configs.base
+    base.pull_base()
+
+    # zero fuel weight
+    base.mass_properties.max_zero_fuel = 0.9 * base.mass_properties.max_takeoff 
+
+    # wing areas
+    for wing in base.wings:
+        wing.areas.wetted   = 2.0 * wing.areas.reference
+        wing.areas.exposed  = 0.8 * wing.areas.wetted
+        wing.areas.affected = 0.6 * wing.areas.wetted
+
+    # diff the new data
+    base.store_diff()
+
+    # ------------------------------------------------------------------
+    #   Landing Configuration
+    # ------------------------------------------------------------------
+    landing = configs.landing
+
+    # make sure base data is current
+    landing.pull_base()
+
+    # landing weight
+    landing.mass_properties.landing = 0.85 * base.mass_properties.takeoff
+
+    # diff the new data
+    landing.store_diff()
+
+    return
 
 # ----------------------------------------------------------------------
 #   Define the Mission
