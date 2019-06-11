a8f832d6dc54c85e20d8ee0b679721b6a688790e
kslam07 <trykslam@gmail.com>
2019-06-11 15:51:21 +0200

    Update def_Collection_ClassII_Sizing.py

--- A22DSE/Models/def_Collection_ClassII_Sizing.py
+++ A22DSE/Models/def_Collection_ClassII_Sizing.py
@@ -124,6 +124,7 @@ def ClassIISizing(Conv):
     
     step = 100
     Conv.ParAnFP.C_L_design, Conv.ParAnFP.tc_w, Conv.ParAnFP.FWP = (
-    TransPlanform.ComputePlanform(Conv, step, Conv.ParAnFP.A, False))
+    TransPlanform.ComputePlanform(Conv, ISA_model, 
+                                  step, Conv.ParAnFP.A, False))
 
     
