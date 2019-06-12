98e56a49837e0f8d864f996520807a4940c14e77
dsegroup22 <50166265+dsegroup22@users.noreply.github.com>
2019-06-12 13:51:54 +0200

    Update fuselageconetorque.py

--- A22DSE/Models/STRUC/current/Class_II/fuselageconetorque.py
+++ A22DSE/Models/STRUC/current/Class_II/fuselageconetorque.py
@@ -8,17 +8,16 @@ Created on Tue Jun 11 16:20:28 2019
 import math as m
 import numpy as np
 
-#import os
-#from pathlib import Path
-#os.chdir(Path(__file__).parents[5])
+import os
+from pathlib import Path
+os.chdir(Path(__file__).parents[5])
 
-#from A22DSE.Parameters.Par_Class_Conventional import Conv
+from A22DSE.Parameters.Par_Class_Conventional import Conv
 
-#anfp = Conv.ParAnFP
-#struc = Conv.ParStruc
-#prop = Conv.ParProp
+
 
 def GetJ(do,di):
+    """Gets the torsional stiffness"""
     J = m.pi*(do**4-di**4)/32
     return J
 
@@ -32,12 +31,18 @@ def GetStressShear(t,do,V,A):
 
 
 
-do = 750 * 10**-3 #m
-t  = np.linspace(0.1,20) * 10**-3
-di = do - 2*t
-T = 5 * 0.5 * 1.225 * 150**2 * 1.2 * 30  #Nm
-V = 0.5 * 1.225 * 150**2 * 1.2 * 30  #N
-A = m.pi * (do/2)**2
-sigma = GetSigma(T,do,di) + GetStressShear(t,do,V,A)
-sigmay = 331 * 10**6
-print(min(t[np.where(sigma<sigmay)]))
+
+def Get_t(Aircraft, do):
+    """ INPUT: do which is the outer diameter"""
+#    anfp = Aircraft.ParAnFP
+#    struc = Aircraft.ParStruc
+#    prop = Aircraft.ParProp
+    layout = Aircraft.ParLayoutConfig
+    t  = np.linspace(0.1,50) * 10**-3
+    di = do - 2*t
+    T = layout.b_v * 0.5 * 1.225 * 150**2 * 1.2 * layout.Svt  #Nm
+    V = 0.5 * 1.225 * 150**2 * 1.2 * layout.Svt  #N
+    A = m.pi * (do/2)**2
+    sigma = GetSigma(T,do,di) + GetStressShear(t,do,V,A)
+    sigmay = 207 * 10**6 #for al 7075 t6
+    return(min(t[np.where(sigma<sigmay)]))
