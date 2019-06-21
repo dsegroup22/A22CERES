1d20df2bb56b4c4a0cf6b746817ca0a063c329f6
lujingyi <50166265+dsegroup22@users.noreply.github.com>
2019-06-21 22:39:56 +0900

    Update flightenvelope.py

--- A22DSE/Models/AnFP/Current/flightenvelope.py
+++ A22DSE/Models/AnFP/Current/flightenvelope.py
@@ -19,17 +19,23 @@ def flightenvelope(Aircraft):
     Vg = Vs*sqrt(-nmin)
     Vc = Aircraft.ParAnFP.V_cruise
     Vd = 1.4*Vc
-    #mug = 2*Aircraft.ParStruc.MTOW*2.205/(Aircraft.ParAnFP.S*10.764)/(rho*0.00194*Aircraft.ParAnFP.MAC*3.281*Aircraft.ParAnFP.C_L_alpha_cruise*32.2)
-    #Kg = 0.7#88*mug/(5.3+mug)
     mug = 2*Aircraft.ParStruc.MTOW/(Aircraft.ParAnFP.S)/(rho*Aircraft.ParAnFP.MAC*Aircraft.ParAnFP.C_L_alpha_cruise*9.80665)
     Kg = 0.88*mug/(5.3+mug)
-    #ngmax = 1+Kg*50*Vc*1.944*Aircraft.ParAnFP.C_L_alpha_cruise/(498*Aircraft.ParStruc.MTOW*2.205/(Aircraft.ParAnFP.S*10.764))
+
     ngmaxc = 1+0.5*rho*Vc*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*15.24/(Aircraft.ParStruc.MTOW/Aircraft.ParAnFP.S)
     ngminc = 1-0.5*rho*Vc*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*15.24/(Aircraft.ParStruc.MTOW/Aircraft.ParAnFP.S)
     ngmaxd = 1+0.5*rho*Vd*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*7.62/(Aircraft.ParStruc.MTOW/Aircraft.ParAnFP.S)
     ngmind = 1-0.5*rho*Vd*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*7.62/(Aircraft.ParStruc.MTOW/Aircraft.ParAnFP.S)
     return(max(ngmaxc,nmax)*1.5,max(ngmaxc,nmax),Vs,Vd)
-
+    
+#    ngmaxc = 1+0.5*rho*Vc*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*15.24/(Aircraft.ParStruc.MTOW*0.63/Aircraft.ParAnFP.S)
+#    ngminc = 1-0.5*rho*Vc*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*15.24/(Aircraft.ParStruc.MTOW*0.63/Aircraft.ParAnFP.S)
+#    ngmaxd = 1+0.5*rho*Vd*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*7.62/(Aircraft.ParStruc.MTOW*0.63/Aircraft.ParAnFP.S)
+#    ngmind = 1-0.5*rho*Vd*eas*Kg*Aircraft.ParAnFP.C_L_alpha_cruise*7.62/(Aircraft.ParStruc.MTOW*0.63/Aircraft.ParAnFP.S)
+#
+#    print(ngmaxc,nmax,nmin,ngmaxc*1.5,Vs,Va,Vc,Vd)
+    
+#====================Plot==============================================    
 #    x_mu1 = np.arange(0,Va,1)
 #    y_mu1 = x_mu1**2*0.5*rho*CLmax*Aircraft.ParAnFP.S/Aircraft.ParStruc.MTOW
 #    x_mu2 = np.arange(Va,Vd,1)
@@ -45,15 +51,47 @@ def flightenvelope(Aircraft):
 #    x_gl1 = np.arange(0,Vc,1)
 #    y_gl1 = (ngminc-1)/Vc*x_gl1+1
 #    x_gl2 = np.arange(Vc,Vd,1)
-#    y_gl2 = (ngmind-ngminc)/(Vd-Vc)*(x_gl2-Vd)+ngmind   
-#    plt.plot(x_mu1,y_mu1)
-#    plt.plot(x_mu2,y_mu2)
-#    plt.plot(x_ml1,y_ml1)
-#    plt.plot(x_ml2,y_ml2)
-#    plt.plot(x_gu1,y_gu1)
-#    plt.plot(x_gu2,y_gu2)
-#    plt.plot(x_gl1,y_gl1)
-#    plt.plot(x_gl2,y_gl2)
+#    y_gl2 = (ngmind-ngminc)/(Vd-Vc)*(x_gl2-Vd)+ngmind
+#    x_gu3 = np.arange(0,Vd,1)
+#    y_gu3 = (ngmaxd-1)/Vd*x_gu3+1
+#    x_gl3 = np.arange(0,Vd,1)
+#    y_gl3 = (ngmind-1)/Vd*x_gl3+1
+#    plt.plot(x_mu1,y_mu1,'r',label='manoeuver limits')
+#    plt.plot(x_mu2,y_mu2,'r')
+#    plt.plot(x_ml1,y_ml1,'r')
+#    plt.plot(x_ml2,y_ml2,'r')
+#    plt.plot(x_gu1,y_gu1,'g',label='gust limits')
+#    plt.plot(x_gu2,y_gu2,'g')
+#    plt.plot(x_gl1,y_gl1,'g')
+#    plt.plot(x_gl2,y_gl2,'g')
+#    plt.plot(x_gu3,y_gu3,'g')
+#    plt.plot(x_gl3,y_gl3,'g')
+#    plt.legend(loc=4,fontsize=15)
+#    plt.xlim(0,300)
+#    plt.ylim(-2,3)
+#    plt.scatter(Vs,1)
+#    plt.annotate('$V_{stall}$',xy=(Vs+2,0.05),fontsize=15)
+#    plt.scatter(Va,nmax)
+#    plt.annotate('$V_{A}$',xy=(Va,0.05),fontsize=15)
+#    plt.scatter(Vc,ngmaxc)
+#    plt.annotate('$V_{C}$',xy=(Vc,0.05),fontsize=15)
+#    plt.scatter(Vd,ngmaxd)
+#    plt.annotate('$V_{D}$',xy=(Vd,0.05),fontsize=15)
+#    plt.axvline(x=Vd,ymin=(nmin+2)/5,ymax=(nmax+2)/5,color='r')
+#    plt.axvline(x=Vd,ymin=(ngmind+2)/5,ymax=(ngmaxd+2)/5,color='g')
+#    plt.axvline(x=Vs,ymin=2/5,ymax=3/5,color='black',linestyle='dashed')
+#    plt.axvline(x=Va,ymax=(nmax+2)/5,ymin=2/5,color='black',linestyle='dashed')
+#    plt.axvline(x=Vc,ymax=(ngmaxc+2)/5,ymin=2/5,color='black',linestyle='dashed')
+#    plt.axvline(x=Vd,ymax=(ngmaxd+2)/5,ymin=2/5,color='black',linestyle='dashed')
+#    plt.axhline(y=1,linewidth=1,color='black')
+#    plt.axhline(y=0,linewidth=1,color='black')
+#    plt.axhline(y=ngmaxc,xmax=Vc/300,linewidth=1,color='black',linestyle='dashed')
+#    plt.axhline(y=nmin,xmax=Vs/300,linewidth=1,color='black',linestyle='dashed')
+#    plt.annotate('$n_{max}$',xy=(0,ngmaxc+0.05),fontsize=15)
+#    plt.annotate('$n_{min}$',xy=(0,nmin+0.05),fontsize=15)
+#    plt.xlabel('Velocity [m/s]',fontsize=15)
+#    plt.ylabel('Load Factor',fontsize=15)
+#    plt.tick_params(labelsize=15)
 #    plt.show()
 
 
